ddf313bb5c0adeecdcd34970db1db07ce47eb5e4
henriezhang <henriezhang@qq.com>
2014-12-05 15:26:17 +0800

    first build

---
+++ src/main/java/com/webdev/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.webdev;
+
+/**
+ * Created by henriezhang on 2014/12/2.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
